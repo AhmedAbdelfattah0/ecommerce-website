9b37e949b0a01ea01240fa0f9cb1b6c6c39bde6d
a-abdelfatah <83225408+a-abdelfatah@users.noreply.github.com>
2025-03-11 14:14:51 +0000

    add WhatsApp button; implement fixed position button for quick access, update footer with social media links, and enhance header logo display

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { BaseComponent } from '../../common/components/base/base.component';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  imports: [BaseComponent.materialModules],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
