9aaf13b10b7fc020a5081a5200d961df0498ef4b
Ahmed Abdelfatah <ahmed.abdelfttah@ikea.com.sa>
2025-03-13 09:35:23 +0200

    refactor: update footer and social gallery components for improved image responsiveness and routing

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { BaseComponent } from '../../common/components/base/base.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [BaseComponent.materialModules],
+  imports: [BaseComponent.materialModules,RouterModule],
   templateUrl: './footer.component.html',
   styleUrl: './footer.component.scss'
 })
