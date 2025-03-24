8593ac5040e99fcfbf2e969f2e06eee8d95e54e5
Ahmed Abdelfatah <ahmed.abdelfttah@ikea.com.sa>
2025-03-24 12:54:06 +0200

    Refactor social gallery styles and clean up unused country filtering logic in ContriesService

--- src/app/services/contries/contries.service.ts
+++ src/app/services/contries/contries.service.ts
@@ -10,43 +10,10 @@ export class ContriesService {
 
   constructor(private http: HttpClient) { }
 
-  private middleEastCountries = [
-    'Bahrain',
-    'Iran',
-    'Iraq',
-    'Israel',
-    'Jordan',
-    'Kuwait',
-    'Lebanon',
-    'Oman',
-    'Qatar',
-    'Saudi Arabia',
-    'United Arab Emirates',
-    'Yemen',
-    'Turkey', 'Egypt', 'Palestine'
-  ];
-  // getContries(): Observable<any> {
-  //   return this.http.get<any>(`countries/countries.php`).pipe(
-  //     map(response => response.data.filter((country: any) =>
-  //       this.middleEastCountries.includes(country.name)
-  //     ))
-  //   );
-  // }
+
+
   getContries(): Observable<any[]> {
-    return this.http.get<any>(`countries/countries.php`)
-    // .pipe(
-    //   map(response => {
-    //     return response.data
-    //       // Optional: if you want to filter only for Middle East countries
-    //       .filter((country: any) => this.middleEastCountries.includes(country.name))
-    //       // Map each country to the desired structure
-    //       .map((country: any) => ({
-    //         name: country.name,
-    //         states: Array.isArray(country.states)
-    //           ? country.states.map((state: any) => ({ name: state.name }))
-    //           : []
-    //       }));
-    //   })
-    // );
+    return this.http.get<any>(`/countries/countries.php`)
+
   }
 }
